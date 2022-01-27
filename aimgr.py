a7cb2c0394b0a49e9e2aba42947f03480325db8c
nevdullc <95062984+nevdullc@users.noreply.github.com>
2022-01-27 15:10:49 -0500

    Update aimgr.py

--- aimgr.py
+++ aimgr.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 
-# aimgr v 1.0
+# aimgr v 1.0 :: nevdullc :: 2022
 # python tool for creating square images from a folder of images
 # squared images are required for import into 'Runway' machine learning app
+# *make sure to put all photos to be operated on in a folder called 'img'
+# run this script from the same folder that the 'img' folder is in
+# output of modified photos will be saved in newly created 'out_aimgr' folder
 
 import os
 import glob
